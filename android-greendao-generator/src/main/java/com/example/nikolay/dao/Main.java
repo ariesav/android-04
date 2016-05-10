8cd6ff5fc677e7884093e30b7764093ba3da5948
Mykola Dymura <nikolay.dymura@gmail.com>
2016-05-10 20:37:39 +0300

    was added greeenda  generator

---
+++ android-greendao-generator/src/main/java/com/example/nikolay/dao/Main.java
@@ -0,0 +1,10 @@
+package com.example.nikolay.dao;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
